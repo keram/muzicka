22777c0e7a56806726923e77a6ae0564fe38c8a6
Marek Labos <keraml@gmail.com>
2012-11-12 15:14:22 +0100

    put secret token to git for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muzicka::Application.config.secret_token = '392366609b05d725a3e4fa9cdb103f463613e269eea125f5623319bb263fe1b2f2c16252cc52d1597eb472604869b91aa8b362f8c57855706ed0c8c4c90e6e78'
